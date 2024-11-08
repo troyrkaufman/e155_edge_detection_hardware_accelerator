a8fcf1025814242e04c5aabe4a3ffc993ff80337
troyrkaufman <tkaufman@g.hmc.edu>
2024-11-08 15:27:02 -0800

    Added lib and src folders to MCU

---
+++ mcu/lib/STM32L432KC_I2C.c
@@ -0,0 +1,5 @@
+// STM32L432KC_I2C.h
+// Header for I2C functions
+
+
+
---
+++ mcu/lib/STM32L432KC_I2C.h
@@ -0,0 +1,9 @@
+// STM32L432KC_I2C.c
+// Source Code for I2C functions
+
+// Initialize I2C
+
+void initI2C();
+
+char I2CsendReceive();
+
