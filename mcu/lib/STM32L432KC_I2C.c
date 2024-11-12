3fb7a2c687855a11c21b586cd6cbbe93890e4ecd
troyrkaufman <tkaufman@g.hmc.edu>
2024-11-11 22:55:27 -0800

    Init I2C should be done. Writing over I2C now

--- mcu/lib/STM32L432KC_I2C.c
+++ mcu/lib/STM32L432KC_I2C.c
@@ -8,35 +8,53 @@
 
 void init_I2C() {
     
-    //1. Enable the I2C CLOCK and GPIO CLOCK
+    // Enable the I2C CLOCK and GPIO CLOCK
     RCC->APB1ENR1 |= _VAL2FLD(RCC_APB1ENR1_I2C1EN, 1);
-    //2. Configure the I2C PINs for ALternate Functions
-    //a) Select Alternate Function in MODER Register
+
+    // Set pclk to 8Mhz by settting the prescalar to 8 (sysclk is 64 Mhz)
+    RCC->CFGR |= _VAL2FLD(RCC_CFGR_PPRE1, 0b110);
+
+    // Enable the GPIOA port
     RCC->AHB2ENR |= _VAL2FLD(RCC_AHB2ENR_GPIOAEN, 1);
-    //b) Select Open Drain Output 
-    GPIOA->OTYPER |= _VAL2FLD(GPIO_OTYPER_OT9, 1);
-    GPIOA->OTYPER |= _VAL2FLD(GPIO_OTYPER_OT10, 1);
-    //c) Select High SPEED for the PINs-----Nope. We will be using the normal 100KHz clk mode
-    
-    //d) Select Pull-up for both the Pins-----USE EXTERNAL PULL UPS (4.7k ~ 10K)
 
-    //e) Configure the Alternate Function in AFR Register
+    // Enable the analog filter
+    I2C1->CR1 &= ~I2C_CR1_ANFOFF;
+
+    // Disable digital filter
+    I2C1->CR1 &= ~I2C_CR1_DNF;
+
+    // Disable clock stretching
+    I2C1->CR1 &= ~I2C_CR1_NOSTRETCH;
+
+    // Enable the I2C peripheral
+    I2C1->CR1 |= I2C_CR1_PE;
+
+    // Select Open Drain Output ... USE EXTERNAL PULL UPS (4.7k ~ 10K)
+    //GPIOA->OTYPER |= _VAL2FLD(GPIO_OTYPER_OT9, 1);
+    //GPIOA->OTYPER |= _VAL2FLD(GPIO_OTYPER_OT10, 1);
+
+    // Configure the GPIO pins in ALT mode
     gpioEnable(GPIO_PORT_A);
-    pinMode(I2C_SCL, GPIO_ALT); // I2C1_SCL
-    pinMode(I2C_SDA, GPIO_ALT);// I2C1_SDA
+    pinMode(I2C_SCL, GPIO_ALT); 
+    pinMode(I2C_SDA, GPIO_ALT); 
 
-    GPIOA->AFR[1] |= _VAL2FLD(GPIO_AFRH_AFSEL10, 0b100);
-    GPIOA->AFR[1] |= _VAL2FLD(GPIO_AFRH_AFSEL9, 0b100);
+    // Set up I2C ALT pin mode 
+    GPIOA->AFR[1] |= _VAL2FLD(GPIO_AFRH_AFSEL10, 4);
+    GPIOA->AFR[1] |= _VAL2FLD(GPIO_AFRH_AFSEL9, 4);
 
-    //3. Reset the I2C 	
+    //3. Reset the I2C Peripheral
     I2C1->CR1 |= _VAL2FLD(I2C_CR1_SWRST, 1); // Reset System
     I2C1->CR1 |= _VAL2FLD(I2C_CR1_SWRST, 0); // Pull from reset
 
     //4. Program the peripheral input clock in I2C_CR2 Register in order to generate correct timings
-    
-    //5. Configure the clock control registers
-    //6. Configure the rise time register
-    //7. Program the I2C_CR1 register to enable the peripheral
+    I2C1->TIMINGR |= _VAL2FLD(I2C_TIMINGR_PRESC, 1);
+    I2C1->TIMINGR |= _VAL2FLD(I2C_TIMINGR_SCLDEL, 0x4);
+    I2C1->TIMINGR |= _VAL2FLD(I2C_TIMINGR_SDADEL, 0x2);
+    I2C1->TIMINGR |= _VAL2FLD(I2C_TIMINGR_SCLH, 0xF);
+    I2C1->TIMINGR |= _VAL2FLD(I2C_TIMINGR_SCLL, 0x13);
+}
 
+char write_I2C(char addr, char index, char data){
+    
 }
 
