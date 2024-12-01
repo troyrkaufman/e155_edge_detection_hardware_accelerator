521e0fd2ba0e538945e20abd84300d18a4a30266
Vikram Krishna <vkrishna@hmc.edu>
2024-12-01 13:19:07 -0500

    updated spi to fix logic and make readable

--- fpga/src/spiRecieve.sv
+++ fpga/src/spiRecieve.sv
@@ -1,27 +1,49 @@
-module spiReceive #(parameter messageBits = 8) (
-  input logic spiClk, nRst,
-  input logic sdi, cs,
-  output logic [messageBits-1:0] writeData,
-  output logic writeEnable
+module spiReceive #(
+    parameter messageBits = 8
+) (
+    input logic spiClk,
+    nRst,
+    input logic sdi,
+    cs,
+    output logic [messageBits-1:0] writeData,
+    output logic writeEnable
 );
-  if (messageBits <= 8) begin
-    logic [2:0] bitCounter, nextBitCounter;
-  end else begin
-    logic [3:0] bitCounter, nextBitCounter;
+  logic [3:0] bitCounter;
+
+  typedef enum logic [1:0] {
+    WAITING,
+    RECEIVING,
+    DONE
+  } spiState_t;
+
+  spiState_t currentState;
+
+  always_comb begin : nextStateLogic
+    if (~nRst) currentState = WAITING;
+    else case (currentState)
+      WAITING: currentState = cs ? RECEIVING : WAITING;
+      RECEIVING: currentState = bitCounter == 0 ? DONE : RECEIVING;
+      DONE: currentState = cs ? RECEIVING : WAITING;
+      default: currentState = WAITING;
+    endcase
   end
-  always_ff @( negedge spiClk, negedge nRst ) begin : mainLogic
-    if (~nRst | ~cs) begin
-      bitCounter <= 0;
-    end else begin
+
+  always_ff @(negedge spiClk) begin : bitCounterLogic
+    if (currentState == RECEIVING) bitCounter <= bitCounter - 1;
+    else bitCounter <= messageBits - 1;
+  end
+
+  logic temp;
+
+  always_ff @(negedge spiClk) begin : writeDataLogic
+    if (currentState == RECEIVING) begin
+      temp <= sdi;
       writeData[bitCounter] <= sdi;
-      bitCounter <= nextBitCounter;
-
-      if (bitCounter == messageBits-1) begin
-        writeEnable <= 1;
-        bitCounter <= 0;
-      end
+    end else begin
+      writeData <= 0;
+      temp <= 0;
     end
-  end  
+  end
 
-  assign nextBitCounter = bitCounter + 1;
+  assign writeEnable = (currentState == DONE);
 endmodule
