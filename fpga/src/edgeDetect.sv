ce74f803a46ef17d53478b1a6470abb233baf021
Vikram Krishna <vkrishna@hmc.edu>
2024-12-04 07:30:39 -0800

    pipelined edge detection

--- fpga/src/edgeDetect.sv
+++ fpga/src/edgeDetect.sv
@@ -1,46 +1,103 @@
 module edgeDetect (
-  // input logic clk, nreset,
-  input logic inputValid,
-  input logic [3:0] pixelData [3][3],
-  output logic [1:0] edgeVal,
-  output logic edgeValValid
+    input logic clk,
+    nreset,
+    input logic inputValid,
+    input logic [3:0] pixelData[3][3],
+    output logic [1:0] edgeVal,
+    output logic edgeValValid
 );
 
-  logic [4:0] row1Val, row3Val; // max val is 15+30+15 = 60
-  logic [4:0] col1Val, col3Val; // max val is 15+30+15 = 60
+  typedef enum logic [2:0] {
+    WAIT,
+    ADD,
+    SUB,
+    SQUARE,
+    SQRT
+  } pipelineState_t;
+  pipelineState_t pipelineState, nextState;
 
-  logic [4:0] rowDiff, colDiff; // max val is 60 - 0 = 60
+  logic [3:0] pixelDataReg[3][3];
+  logic [4:0] row1ValReg, row3ValReg, nextRow1Val, nextRow3Val;  // max val is 15+30+15 = 60
+  logic [4:0] col1ValReg, col3ValReg, nextCol1Val, nextCol3Val;  // max val is 15+30+15 = 60
 
-  logic [11:0] rowSquare, colSquare; // max val is 60^2 = 3600 = 12'b111000010000
+  logic [4:0] rowDiffReg, colDiffReg, nextRowDiff, nextColDiff;  // max val is 60 - 0 = 60
 
-  logic [12:0] squareSum; // max val is 3600 + 3600 = 7200 = 13'b111000100000
+  logic [11:0] rowSquare, colSquare;  // max val is 60^2 = 3600 = 12'b111000010000
 
-  logic [6:0] sqrtVal; // max val is sqrt(7200) = 85 = 7'b1010101
+  logic [12:0] squareSumReg, nextSquareSum;  // max val is 3600 + 3600 = 7200 = 13'b111000100000
+
+  logic [6:0] sqrtValReg, nextSqrtVal;  // max val is sqrt(7200) = 85 = 7'b1010101
+  logic [1:0] nextEdgeVal;
 
   sqrtRom sqrtRom (
-    .squareSum(squareSum),
-    .sqrtVal(sqrtVal)
+      .squareSum(squareSumReg),
+      .sqrtVal  (nextSqrtVal)
   );
 
   bitCompress bitCompress (
-    .inVal(sqrtVal),
-    .outVal(edgeVal)
+      .inVal (sqrtValReg),
+      .outVal(nextEdgeVal)
   );
 
-  always_comb begin : rowColCalcs
-    row1Val = pixelData[0][0] + pixelData[0][1] << 2 + pixelData[0][2];
-    row3Val = pixelData[2][0] + pixelData[2][1] << 2 + pixelData[2][2];
-    col1Val = pixelData[0][0] + pixelData[1][0] << 2 + pixelData[2][0];
-    col3Val = pixelData[0][2] + pixelData[1][2] << 2 + pixelData[2][2];
-
-    rowDiff = row1Val > row3Val ? row1Val - row3Val : row3Val - row1Val;
-    colDiff = col1Val > col3Val ? col1Val - col3Val : col3Val - col1Val;
-
-    rowSquare = rowDiff * rowDiff; // change this to use the IP
-    colSquare = colDiff * colDiff;
-
-    squareSum = rowSquare + colSquare;
+  always_ff @(posedge clk, negedge nreset) begin : stateLogic
+    if (~nreset) begin
+      pipelineState <= WAIT;
+    end else begin
+      pipelineState <= nextState;
+    end
   end
 
-  assign edgeValValid = inputValid;
+  always_comb begin : nextStateLogic
+    case (pipelineState)
+      WAIT: nextState = inputValid ? ADD : WAIT;
+      ADD: nextState = SUB;
+      SUB: nextState = SQUARE;
+      SQUARE: nextState = SQRT;
+      SQRT: nextState = WAIT;
+      default: nextState = WAIT;
+    endcase
+  end
+
+  always_ff @(posedge clk, negedge nreset) begin : regLogic
+    if (~nreset) begin
+      pixelDataReg <= '{default: 0};
+      row1ValReg <= 0;
+      row3ValReg <= 0;
+      col1ValReg <= 0;
+      col3ValReg <= 0;
+      rowDiffReg <= 0;
+      colDiffReg <= 0;
+      squareSumReg <= 0;
+      sqrtValReg <= 0;
+      edgeVal <= 0;
+    end else begin
+      pixelDataReg <= pixelData;
+      row1ValReg <= nextRow1Val;
+      row3ValReg <= nextRow3Val;
+      col1ValReg <= nextCol1Val;
+      col3ValReg <= nextCol3Val;
+      rowDiffReg <= nextRowDiff;
+      colDiffReg <= nextColDiff;
+      squareSumReg <= nextSquareSum;
+      sqrtValReg <= nextSqrtVal;
+      edgeVal <= nextEdgeVal;
+    end
+  end
+
+  always_comb begin : rowColCalcs
+    nextRow1Val = pixelDataReg[0][0] + (pixelDataReg[0][1] << 1) + pixelDataReg[0][2];
+    nextRow3Val = pixelDataReg[2][0] + (pixelDataReg[2][1] << 1) + pixelDataReg[2][2];
+    nextCol1Val = pixelDataReg[0][0] + (pixelDataReg[1][0] << 1) + pixelDataReg[2][0];
+    nextCol3Val = pixelDataReg[0][2] + (pixelDataReg[1][2] << 1) + pixelDataReg[2][2];
+
+    nextRowDiff = row1ValReg > row3ValReg ? row1ValReg - row3ValReg : row3ValReg - row1ValReg;
+    nextColDiff = col1ValReg > col3ValReg ? col1ValReg - col3ValReg : col3ValReg - col1ValReg;
+
+    rowSquare = rowDiffReg * rowDiffReg;  // change this to use the IP
+    colSquare = colDiffReg * colDiffReg;
+
+    nextSquareSum = rowSquare + colSquare;
+  end
+
+  assign edgeValValid = pipelineState == SQRT;
 endmodule
--- fpga/src/top.sv
+++ fpga/src/top.sv
@@ -1,7 +1,7 @@
 module top (
     input logic spiClk,
     sdi,
-    ce,
+    ncs,
     input logic nreset,
     output logic hSync,
     vSync,
@@ -11,75 +11,77 @@ module top (
     green,
     blue
 );
-    logic oscClk, mainClk, vgaClk;
-    logic [1:0] outVal, edgeVal;
-    logic [11:0] spiData;
-    logic pixelDataValid, edgeValid;
+  logic oscClk, mainClk, vgaClk;
+  logic [1:0] outVal, edgeVal;
+  logic [11:0] spiData;
+  logic pixelDataValid, edgeValid;
 
-    logic [18:0] addressRead, addressWrite;
+  logic [18:0] addressRead, addressWrite;
 
-    logic [3:0] pixelData[3][3];
+  logic [3:0] pixelData[3][3];
 
-    HSOSC #(
-        .CLKHF_DIV("0b10")
-    ) hsosc (
-        .CLKHFEN(1'b1),
-        .CLKHFPU(1'b1),
-        .CLKHF  (oscClk)
-    );
+  HSOSC #(
+      .CLKHF_DIV("0b10")
+  ) hsosc (
+      .CLKHFEN(1'b1),
+      .CLKHFPU(1'b1),
+      .CLKHF  (oscClk)
+  );
 
 
-    pll50p35 vgaPll (
-        .ref_clk_i(oscClk),
-        .rst_n_i(nreset),
-        .outcore_o(mainClk),
-        .outglobal_o()
-    );
+  pll50p35 vgaPll (
+      .ref_clk_i(oscClk),
+      .rst_n_i(nreset),
+      .outcore_o(mainClk),
+      .outglobal_o()
+  );
 
-    spramController spramCont (
-        .mainClk(mainClk),
-        .nreset(nreset),
-        .addressRead(addressRead),
-        .addressWrite(addressWrite),
-        .writeData(edgeVal),
-        .load(edgeValid),
-        .vgaClk(vgaClk),
-        .outVal(outVal)
-    );
+  spramController spramCont (
+      .mainClk(mainClk),
+      .nreset(nreset),
+      .addressRead(addressRead),
+      .addressWrite(addressWrite),
+      .writeData(edgeVal),
+      .load(edgeValid),
+      .vgaClk(vgaClk),
+      .outVal(outVal)
+  );
 
-    spiController spiCont(
-        .mainClk(mainClk),
-        .spiClk(spiClk),
-        .nreset(nreset),
-        .sdi(sdi),
-        .ncs(ce),
-        .pixelData(pixelData),
-        .pixelDataValid(pixelDataValid),
-        .spiXVal(addressWrite[9:0]),
-        .spiYVal(addressWrite[18:10])
-    );
+  spiController spiCont (
+      .mainClk(mainClk),
+      .spiClk(spiClk),
+      .nreset(nreset),
+      .sdi(sdi),
+      .ncs(ncs),
+      .pixelData(pixelData),
+      .pixelDataValid(pixelDataValid),
+      .spiXVal(addressWrite[9:0]),
+      .spiYVal(addressWrite[18:10])
+  );
 
-    vgaController vgaCont (
-        .vgaClk(vgaClk),
-        .rst(nreset),
-        .hSync(hSync),
-        .vSync(vSync),
-        .syncB(syncB),
-        .blankB(blankB),
-        .hCount(addressRead[9:0]),
-        .vCount(addressRead[18:10])
-    );
+  vgaController vgaCont (
+      .vgaClk(vgaClk),
+      .rst(nreset),
+      .hSync(hSync),
+      .vSync(vSync),
+      .syncB(syncB),
+      .blankB(blankB),
+      .hCount(addressRead[9:0]),
+      .vCount(addressRead[18:10])
+  );
 
-    edgeDetect edgeDetectInst (
-        .inputValid(pixelDataValid),
-        .pixelData(pixelData),
-        .edgeVal(edgeVal),
-        .edgeValValid(edgeValid)
-    );
+  edgeDetect edgeDetectInst (
+      .clk(mainClk),
+      .nreset(nreset),
+      .inputValid(pixelDataValid),
+      .pixelData(pixelData),
+      .edgeVal(edgeVal),
+      .edgeValValid(edgeValid)
+  );
 
-    assign red   = syncB ? 2'b00 : outVal;
-    assign green = syncB ? 2'b00 : outVal;
-    assign blue  = syncB ? 2'b00 : outVal;
+  assign red   = syncB ? 2'b00 : outVal;
+  assign green = syncB ? 2'b00 : outVal;
+  assign blue  = syncB ? 2'b00 : outVal;
 
 
 endmodule
