5ebea32805bc0644f0224b67592877560bc2729c
Vikram Krishna <vkrishna@hmc.edu>
2024-12-03 20:31:44 -0800

    fixed vga logic (again)

--- fpga/src/vgaController.sv
+++ fpga/src/vgaController.sv
@@ -17,7 +17,7 @@ module vgaController #(
     syncB,
     blankB,
     output logic [9:0] hCount,
-    vCount
+    output logic [8:0] vCount
 );
 
   always @(posedge vgaClk, posedge rst) begin
@@ -25,20 +25,24 @@ module vgaController #(
       hCount <= 0;
       vCount <= 0;
     end else begin
-      hCount++;
+      hCount <= hCount + 1;
       if (hCount == HMAX) begin
         hCount <= 0;
-        vCount++;
+        vCount <= vCount + 1;
         if (vCount == VMAX) begin
           vCount <= 0;
         end
       end
     end
-
-    assign hSync = (hCount >= HSYNC) && (hCount < HSYNC + HBP);
-    assign vSync = (vCount >= VSYNC) && (vCount < VSYNC + VBP);
-    assign syncB = hSync && vSync;
-    assign blankB = hSync || vSync;
   end
+
+  assign hSync  = ~((hCount >= (HACTIVE + HFP)) & (hCount < HACTIVE + HFP + HSYNC));
+  // not hcount >= 656 aand hcount < 752
+  // hcount < 656 or hcount >= 752
+  assign vSync  = ~((vCount >= (VACTIVE + VFP)) & (vCount < VACTIVE + VFP + VSYNC));
+  // not vcount >= 491 and vcount < 493
+  // vcount < 491 or vcount >= 493
+  assign syncB  = hCount < HACTIVE & vCount < VACTIVE;
+  assign blankB = hSync & vSync;
 
 endmodule
